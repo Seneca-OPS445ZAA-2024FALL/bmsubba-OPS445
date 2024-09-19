db62b7a5cd038e442e2bca455f69ee3504b2afbf
Budhi Subba <bmsubba@myseneca.ca>
2024-09-18 22:41:38 -0400

    Added lab1d

---
+++ lab1/lab1d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Defining the objects 'x', 'y', and 'z'
+x = 10
+y = 2
+z = 5
+
+# Performing the calculation
+result = x + y * z
+
+# Printing the required output
+print(f"{x} + {y} * {z} = {result}")
